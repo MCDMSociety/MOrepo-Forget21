40204664fe7342583329a13430f1c116cea70a12
Lars Relund Nielsen <junk@relund.dk>
2021-06-23 16:17:31 +0200

    Validate ND sets

---
+++ results/paper_tab_fig/validate_nd_sets.R
@@ -0,0 +1,38 @@
+## Validate if ND sets the same for all solved instances
+
+library(fs)
+library(here)
+library(tidyverse)
+
+getNDSet <- function(path) {
+  dat <- read_csv(path, col_types = cols())
+  return(dat)
+}
+
+dat <- read_csv(here("results", "convert", "csv", "resultsMain.csv")) %>% 
+  mutate(instance_name = str_remove(instance, "....$"),
+         instance = instance_name,
+         class = str_replace(instance_name, "^.*?-(.*?)_(.*)", "\\1"),
+         alg_config = str_c("\\", configLB, "-\\", toupper(configValSplit))) %>% 
+  filter(solved == 1) %>% 
+  group_by(instance_name) %>% 
+  filter(n() > 1) %>% 
+  nest() %>%
+  ungroup() %>% 
+  # slice_head(n=2) %>% 
+  mutate(validated = map_lgl(data,  function(df) {
+    for (i in 1:nrow(df)) {
+      tmp <- getNDSet(here("results/convert/csv/statFiles/UB", str_c(df$instance[i], "_", df$configLB[i], "_", df$configValSplit[i], ".txt")))
+      if (i == 1) {
+        nDSet = tmp
+        next
+      }
+      if (nrow(setdiff(nDSet[,1:(ncol(nDSet)-1)], tmp[,1:(ncol(tmp)-1)])) > 0) {
+        warning("Different nondominated sets for instance ", df$instance[i])
+        return(FALSE)
+      }
+    }
+    return(TRUE)
+  }))
+which(!dat$validated)
+warnings()
